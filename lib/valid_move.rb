a77648ac085fcad0886d4ed6cba51f7f1764906c
Xavier C Barnes <xcbarnes@gmail.com>
2019-02-24 15:54:00 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
-def valid_move(index)
+def valid_move(board,index)
   if 
-    !position_taken?(index) && index === (0..8)
+    !position_taken?(board,index) && index === (0..8)
     TRUE
   else 
     FALSE
